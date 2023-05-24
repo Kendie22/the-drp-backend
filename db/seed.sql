320b79a9c682f6b4709a7944a77881e0014cd050
Kerridene Small <Kerridenesmall@gmail.com>
2023-05-23 23:52:15 -0400

    updated seed

--- db/seed.sql
+++ db/seed.sql
@@ -10,7 +10,10 @@ VALUES
     ('Carly', 'Cushnie','Cushine', 10, false, 'USA', 'High-end', 'https://cdn.shopify.com/s/files/1/1031/5525/articles/Carly_Cushnie_Portraits_043-2_1024x1024.jpg?v=1606248154' ),
     ('Ozwald', 'Boateng', 'Ozwald Boateng', 30, false, 'United Kingdom', 'High-end', 'https://images.squarespace-cdn.com/content/v1/56ec32ddc2ea51bd674b7933/1467753781977-T5VO8KPZ2D21TDT722PV/image-asset.jpeg?format=1000w'),
     ('Grace', 'Wales Bonner', 'Wales Bonner', 5, true, 'United Kingdom', 'Luxury','https://www.washingtonpost.com/resizer/r1OEGYp5EFSGM9HWld7JHg158rY=/arc-anglerfish-washpost-prod-washpost/public/TBARKWLODRA7BMSLKXZQYPUR7Q.jpg'),
-    ('Christopher', 'John Rogers', 'Christopher John Rogers', 5, false, 'United States', 'High-end', 'https://pyxis.nymag.com/v1/imgs/76c/c7b/b2ea8822ca4bbc2d900bd34e2d6100c227-CJR-Shikeith-01.rvertical.w570.jpg');
+    ('Christopher', 'John Rogers', 'Christopher John Rogers', 5, false, 'United States', 'High-end', 'https://pyxis.nymag.com/v1/imgs/76c/c7b/b2ea8822ca4bbc2d900bd34e2d6100c227-CJR-Shikeith-01.rvertical.w570.jpg'),
+    ('Stella', 'Jean', 'Stella Jean', 10, true, 'Italy', 'Mid-end', 'https://hips.hearstapps.com/hmg-prod/images/designer-stella-jean-poses-during-the-vogue-fashion-dubai-news-photo-1593426289.jpg'), 
+    ('Virgil', 'Abloh', 'Off-White & Louis Vuitton Men', 10, false, 'United States', 'Luxury', 'https://images.businessoffashion.com/profiles/asset/1546801818140655/virgil-abloh_christian-anwander-1546802046038668.jpg?auto=format%2Ccompress&fit=crop&h=360&w=660'),
+    ('LaQuan', 'Smith', 'LaQuan Smith', 10, false, 'United States', 'High-end', 'https://media.voguebusiness.com/photos/613876fcb5d1d70d84084dc0/2:3/w_2240,c_limit/laquan-smith-voguebus-laquan-smith-sep-21-story.jpg');
 
 INSERT INTO clothing (designer_id, style, color, size, is_recycled_item, material, price, ranking, image)
 VALUES 
@@ -20,4 +23,7 @@ VALUES
     (4, 'Jumpsuit', 'Black and White', 12, false, 'Silk', 493, 4, 'https://media.bergdorfgoodman.com/f_auto,q_auto/01/bg_3034017_100188_m'),
     (5, 'Shirt', 'Brown', 8, true, 'Leather', 399, 5, 'https://images.squarespace-cdn.com/content/v1/6169c88e4d8ab0099aaf6e31/1676472402138-RB30IKYH1FO960B3PSP9/10+copy.jpg?format=750w'),
     (6, 'Dress', 'Mutli-colored', 9, true, 'Cotton', 500, 6, 'https://www.net-a-porter.com/variants/images/1647597289730919/in/w920_q60.jpg'),
-    (7, 'Dress', 'Multi-colored', 4, false, 'Knit', 1125, 7, 'https://media.bergdorfgoodman.com/f_auto,q_auto:low,ar_5:7,c_fill,dpr_2.0,w_720/01/bg_4440880_100010_m');
+    (7, 'Dress', 'Multi-colored', 4, false, 'Knit', 1125, 7, 'https://media.bergdorfgoodman.com/f_auto,q_auto:low,ar_5:7,c_fill,dpr_2.0,w_720/01/bg_4440880_100010_m'),
+    (8, 'Dress', 'White', 5, true, 'Knit', 945, 8, 'https://m.media-amazon.com/images/G/01/Shopbop/p/prod/products/sjean/sjean302351c925/sjean302351c925_1680014554495_2-0._UX846_FMwebp_.jpg'),
+    (9, 'Hoodie', 'Black', 5, false, 'Cotton', 1305, 14, 'https://cdn-images.farfetch-contents.com/19/77/93/70/19779370_44494120_1000.jpg?c=3'),
+    (10, 'Shirt', 'Black and White', 5, false, 'Leather', 304, 3, 'https://is4.fwrdassets.com/images/p/fw/z/LQNS-WS40_V1.jpg');
